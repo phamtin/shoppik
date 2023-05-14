4eafef313fe9086e1aaa47fef98491f260c05c30
tin.pham <tinphamtp@gmail.com>
2023-05-14 15:52:05 +0700

    adjust ui

--- apps/api/ecosystem.config.js
+++ apps/api/ecosystem.config.js
@@ -1,23 +1,21 @@
-const config = {
-    apps: [
-        {
-            name: 'veneno',
-            script: 'npm',
-            args: 'start',
-            time: true,
-            exec_mode: 'fork', // need explicitly declare mode otherwise it will fallback to cluster mode and cause infinite reload
-            instances: 1,
-            autorestart: true,
-            watch: false,
-            max_memory_restart: '1G',
-            env: {
-                NODE_ENV: 'development',
-            },
-            env_production: {
-                NODE_ENV: 'production',
-            },
-        },
-    ],
+module.exports = {
+	apps: [
+		{
+			name: 'veneno',
+			script: 'npm',
+			args: 'start',
+			time: true,
+			exec_mode: 'fork', // need explicitly declare mode otherwise it will fallback to cluster mode and cause infinite reload
+			instances: 1,
+			autorestart: true,
+			watch: false,
+			max_memory_restart: '1G',
+			env: {
+				NODE_ENV: 'development',
+			},
+			env_production: {
+				NODE_ENV: 'production',
+			},
+		},
+	],
 };
-
-export default config;
